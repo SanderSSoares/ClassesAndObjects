1ae92812646a8f50affd349e8f68c4c174979095
Sander2022164 <2022164@student.cct.ie>
2023-10-04 13:14:58 +0100

    New class person created into package

---
+++ src/classesandobjects/Person.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classesandobjects;
+
+import Shapes.Triangle;
+
+/**
+ *
+ * @author sande
+ */
+public class Person {
+    
+    double Height;
+    double weight;
+    String firstName;
+    String surName;
+    boolean isProgrammer;
+    Triangle favTriangle;
+    
+    
+}
