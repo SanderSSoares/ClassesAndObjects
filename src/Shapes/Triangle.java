5b2116414e6de49b282c32957f1da3f500c88ace
Sander2022164 <2022164@student.cct.ie>
2023-10-04 13:54:02 +0100

    Encapsulated the triangle
    Validation added in case negative number is requested by the user

--- src/Shapes/Triangle.java
+++ src/Shapes/Triangle.java
@@ -10,11 +10,40 @@ package Shapes;
  */
 public class Triangle {
     
-    int side1;
-    int side2;
-    int side3;
+    private int side1;
+    private int side2;
+    private int side3;
     
     //Atribuutes are properties/characteristics of the object
     //Behaviour is the mothod linked to the objects
+
+    public int getSide1() {
+        return side1;
+    }
+
+    public void setSide1(int side1) {
+        if (side1>0) {
+        this.side1 = side1;
+        }else{
+            System.out.println("Can not set side1 of a triangle to be non-positive.");
+        }
+    }
+
+    public int getSide2() {
+        return side2;
+    }
+
+    public void setSide2(int side2) {
+        this.side2 = side2;
+    }
+
+    public int getSide3() {
+        return side3;
+    }
+
+    public void setSide3(int side3) {
+        this.side3 = side3;
+    }
+    
     
 }
